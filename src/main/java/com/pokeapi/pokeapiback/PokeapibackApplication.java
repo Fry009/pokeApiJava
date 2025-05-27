82fbff003a28649730e637354123aa7e12121516
fmdiazmontes87 <fmdiazmontes87@gmail.com>
2025-05-27 16:33:19 +0200

    first commit

---
+++ src/main/java/com/pokeapi/pokeapiback/PokeapibackApplication.java
@@ -0,0 +1,13 @@
+package com.pokeapi.pokeapiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeapibackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeapibackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokeapi/pokeapiback/PokeapibackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokeapi.pokeapiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeapibackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
